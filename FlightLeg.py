01695daaf6778b7628cfc233a2e2aa47e227b485
kujawa-konrad <106272820+kujawa-konrad@users.noreply.github.com>
2022-08-09 18:23:11 +0200

    Add files via upload

---
+++ FlightLeg.py
@@ -0,0 +1,76 @@
+import sqlite3
+import csv
+
+conn = sqlite3.connect('flightdb.sqlite')
+cur = conn.cursor()
+
+cur.execute('''
+DROP TABLE IF EXISTS FlightLeg''')
+
+cur.execute('''
+CREATE TABLE FlightLeg (
+    id INTEGER PRIMARY KEY AUTOINCREMENT, 
+    tailNumber TEXT, 
+    sourceAirportCode TEXT, 
+    destinationAirportCode TEXT, 
+    sourceCountryCode TEXT, 
+    destinationCountryCode TEXT, 
+    departureTimeUtc TEXT, 
+    landingTimeUtc TEXT);''')
+
+with open('flightlegs.csv', 'r', encoding='utf-8-sig') as f:
+    reader = csv.DictReader(f, delimiter=';')
+    to_db = [(i['tailNumber'], 
+    i['source_airport_code'], 
+    i['source_country_code'], 
+    i['destination_airport_code'], 
+    i['destination_country_code'], 
+    i['departure_time'], 
+    i['landing_time']) for i in reader]
+
+
+
+cur.executemany('''
+INSERT INTO FlightLeg (
+    tailNumber, 
+    sourceAirportCode, 
+    sourceCountryCode, 
+    destinationAirportCode, 
+    destinationCountryCode, 
+    departureTimeUtc, 
+    landingTimeUtc) 
+    VALUES (?, ?, ?, ?, ?, ?, ?);''', to_db)
+
+conn.commit()
+
+cur.executescript('''
+ALTER TABLE FlightLeg ADD flightDuration INTEGER;
+ALTER TABLE FlightLeg ADD flightType TEXT''')
+
+conn.commit()
+
+cur.executescript('''
+UPDATE FlightLeg SET flightDuration = ROUND((JULIANDAY(landingTimeUtc) - JULIANDAY(departureTimeUtc)) * 1440);
+UPDATE FlightLeg SET flightType = CASE WHEN sourceCountryCode = destinationCountryCode THEN "D" ELSE "I" END''')
+
+conn.commit()
+
+flights = cur.execute('''
+SELECT tailNumber, count(tailNumber) FROM FlightLeg GROUP BY tailNumber''').fetchall()
+max_flights = max(flights, key=lambda item:item[1])
+print(max_flights)
+
+longest_flights = cur.execute('''
+SELECT tailNumber, departureTimeUtc, flightType, MAX(flightDuration) FROM FlightLeg GROUP BY flightType''').fetchall()
+print(longest_flights)
+
+shortest_flights = cur.execute('''
+SELECT tailNumber, departureTimeUtc, flightType, MIN(flightDuration) FROM FlightLeg GROUP BY flightType''').fetchall()
+print(shortest_flights)
+
+print(len(flights))
+print(len(longest_flights))
+print(len(shortest_flights))
+
+conn.close()
+
